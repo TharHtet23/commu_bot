6fbd61acb55129852cd70733c4064e3d2a4a3cb6
TharHtet236401 <tharhteta3@gmail.com>
2024-09-08 09:52:06 +0700

    first comit working bot

---
+++ index.js
@@ -0,0 +1,27 @@
+import TelegramBot from 'node-telegram-bot-api';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
+
+bot.on('callback_query', (query) => {
+  const chatId = query.message.chat.id;
+  const data = query.data; // Data from the web app
+
+  // Send a message back to the user in the chat
+  bot.sendMessage(chatId, `Received: ${data}`);
+});
+
+// Inline button to open the web app
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;gi
+
+  bot.sendMessage(chatId, 'Open the web app:', {
+    reply_markup: {
+      inline_keyboard: [
+        [{ text: 'Open Web App', web_app: { url: 'http://127.0.0.1:3000/' } }]
+      ]
+    }
+  });
+});
